77434d2b6af1604e21a70dec8b429ec5e42cd746
rich1126 <31447594+rich1126@users.noreply.github.com>
2018-11-13 08:12:27 -0800

    Update MathJaxLocal.js

--- assets/MathJaxLocal.js
+++ assets/MathJaxLocal.js
@@ -1,8 +1,10 @@
 MathJax.Hub.Config({
   jax: ["input/TeX", "output/HTML-CSS"],
   tex2jax: {
-    inlineMath: [['$','$'], ['\\(','\\)']],
-    processEscapes: true
+    inlineMath: [['$', '$'],['\\(','\\)']],
+    displayMath: [['$$', '$$'],['\\[','\\]']],
+    processEscapes: true,
+    skipTags: ['script', 'noscript', 'style', 'textarea', 'pre', 'code']
   },
   messageStyle: "none",
   "HTML-CSS": { preferredFont: "TeX", availableFonts: ["STIX","TeX"] },
